c11d3e4298ad5cf13bccdc30f53dca50b0b7bc4a
HafezFaruk <hmomarfaruk1454@gmail.com>
2022-02-07 20:35:44 +0600

    number decrease and increase added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 const Form = () => {
-    const num = 1
+  const [num, setNum] = useState(1)
+
+  const increase = ()=>{
+    setNum(num+1)
+  } 
+  const decrease = () => {
+    if (num < 0) {
+      setNum(num - 1);
+    } else {
+      setNum(0);
+    }
+  }; 
+
   return (
-    <div className="form-bg py-5">
+    <div className="form-bg">
       <div className="bg-tran">
         <h4 className="text-uppercase text-white">join the game</h4>
-        <div className="d-flex justify-content-center align-items-center">
-          <button className="my-btn"></button>
-          <input className="my-input" placeholder="1st Gen" type="text" />
+        <div className="stGen">
+          <div className="d-flex justify-content-center align-items-center">
+            <button className="my-btn"></button>
+            <input className="my-input" placeholder="1st Gen" type="text" />
+          </div>
         </div>
+        <br />
         <span>1000/1000 Minted</span>
         <div>
-          <span className="my-color increment">-</span>
-          <input className="midel-input" type="text" value={num}/>
-          <span className="my-color">+</span>
+          <button onClick={decrease} className="my-color increment">
+            -
+          </button>
+          <input className="midel-input" type="text" value={num} />
+          <button onClick={increase} className="my-color">
+            +
+          </button>
         </div>
         <p>Current cost: per token</p>
         <span className="connect-wallet">Disconnect</span>
